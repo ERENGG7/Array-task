bd9eae9a730c69aa16389ad17cd6ff5937a52861
Eren <erengench12@gmail.com>
2026-07-02 12:15:35 +0300

    add: script for quick commit

---
+++ gcommit.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+while true; do
+    read "commit_message?enter commit message"
+
+    if [[ -z "$commit_message" ]];then
+        echo "Commit message can't be empty."
+        continue
+    fi
+    git add .
+    git commit -m "$commit_message"
+    break
+
+done
+
+ 
--- gitinit.sh
+++ gitinit.sh
@@ -1,6 +1,13 @@
 #!/bin/zsh
 
+
+if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+    echo "Git is initialized in this directory."
+    exit 0
+fi
+
 echo "Initializing git repository ..."
-git init .
-git add .
-git commit -m "Initial commit"
+    git init .
+    git add .
+    git commit -m "Initial commit"  
+exit 0
