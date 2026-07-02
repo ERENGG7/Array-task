ff06e9a50aa084bf2b0b3365c5f6af693f00b6b7
Eren <erengench12@gmail.com>
2026-07-02 13:09:07 +0300

    fix: read message

--- gcommit.sh
+++ gcommit.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 while true; do
-    read "commit_message?enter commit message"
+    read "commit_message?enter commit message: "
 
     if [[ -z "$commit_message" ]];then
         echo "Commit message can't be empty."
